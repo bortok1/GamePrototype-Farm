fc0f6900f40d77917b62b1841c11d87685eced1c
bortok1 <94564592+bortok1@users.noreply.github.com>
2023-03-15 00:37:30 +0100

    UI timer

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
@@ -7,10 +8,10 @@ using UnityEngine.UI;
 
 public class Map : MonoBehaviour
 {
-    [SerializeField] [Range(0, 100)] int areaSizeX;
-    [SerializeField] [Range(0, 100)] int areaSizeY;
-    [SerializeField] [Range(0, 100)] int numberOfImpassable;
-    [SerializeField] [Range(0, 100)] int numberOfOvergrow;
+    [SerializeField] [Range(0, 100)] int areaSizeX = 20;
+    [SerializeField] [Range(0, 100)] int areaSizeY = 10;
+    [SerializeField] [Range(0, 100)] int numberOfImpassable = 15;
+    [SerializeField] [Range(0, 100)] int numberOfOvergrow = 30;
     [SerializeField] GameObject tileGameObject;
 
     [SerializeField] private RawImage sliderPlayer1;
@@ -19,32 +20,43 @@ public class Map : MonoBehaviour
     [SerializeField] private TextMeshProUGUI textPlayer1;
     [SerializeField] private TextMeshProUGUI textPlayer2;
     
-    private EState [,] _tiles;
+    [SerializeField] private TextMeshProUGUI textTimer;
+    [SerializeField] [Range(0, 300)] private float timePerRound = 120;
+    [SerializeField] [Range(0, 300)] private float timeBetweenRounds = 5;
+    
     private List<TileState> _tileStates = new();
     private readonly List<Vector2Int> _availablePos = new();
-    private float _tileSize;
 
     private float _theThing;
+    private float _timer;
+    private float _smallTimer;
 
     void Start()
     {
+        _timer = timePerRound;
+        _smallTimer = timeBetweenRounds;
+        
         _theThing = areaSizeX * areaSizeY - numberOfImpassable;
         
         areaSizeX += 2; // add space for
         areaSizeY += 2; // barier around map
         
-        _tileSize = tileGameObject.transform.localScale.x;
-        _tiles = new EState[areaSizeX, areaSizeY];
+        GenerateMap();
+    }
+
+    private void GenerateMap()
+    {
+        float tileSize = tileGameObject.transform.localScale.x;
+        EState [,] tiles = new EState[areaSizeX, areaSizeY];
 
         for (int i = 0; i < areaSizeX; i++)
         {
             for (int j = 0; j < areaSizeY; j++)
             {
-                _tiles[i, j] = EState.Empty;
+                tiles[i, j] = EState.Empty;
                 if (i == 0 || i == areaSizeX - 1 || j == 0 || j == areaSizeY - 1) // bariers around map
-                    _tiles[i, j] = EState.Impassable;
-                else if ((i > 3 && i < areaSizeX - 4) || !(j <= (areaSizeY / 2) + 1 && j >= (areaSizeY / 2) - 2))
-                    // permanent empty zone
+                    tiles[i, j] = EState.Impassable;
+                else if ((i > 3 && i < areaSizeX - 4) || !(j <= (areaSizeY / 2) + 1 && j >= (areaSizeY / 2) - 2)) // permanent empty zone
                     _availablePos.Add(new Vector2Int(i, j));
                 //else
                   //   tiles[i, j] = EState.Grown;     // use to debug permanent empty zone
@@ -57,7 +69,7 @@ public class Map : MonoBehaviour
             int chosenPos = Random.Range(0, _availablePos.Count);
             int rndX = _availablePos[chosenPos].x;
             int rndZ = _availablePos[chosenPos].y;
-            _tiles[rndX, rndZ] = EState.Impassable;
+            tiles[rndX, rndZ] = EState.Impassable;
             _availablePos[chosenPos] = _availablePos.Last();
             _availablePos.RemoveAt(_availablePos.Count - 1);
         }
@@ -68,7 +80,7 @@ public class Map : MonoBehaviour
             int chosenPos = Random.Range(0, _availablePos.Count);
             int rndX = _availablePos[chosenPos].x;
             int rndZ = _availablePos[chosenPos].y;
-            _tiles[rndX, rndZ] = EState.Overgrown;
+            tiles[rndX, rndZ] = EState.Overgrown;
             _availablePos[chosenPos] = _availablePos.Last();
             _availablePos.RemoveAt(_availablePos.Count - 1);
         }
@@ -77,19 +89,42 @@ public class Map : MonoBehaviour
         {
             for (int j = 0; j < areaSizeY; j++)
             {
-                GameObject newTile = Instantiate(tileGameObject, new Vector3(i*_tileSize, 0, j*_tileSize), Quaternion.identity);
+                GameObject newTile = Instantiate(tileGameObject, new Vector3(i*tileSize, 0, j*tileSize), Quaternion.identity);
                 newTile.transform.SetParent(this.transform);
                 TileState newTileState = newTile.GetComponent<TileState>();
-                newTileState.SetState(_tiles[i, j]);
+                newTileState.SetState(tiles[i, j]);
                 _tileStates.Add(newTileState);
             }
         }
     }
 
+    private void ClearMap()
+    {
+        foreach (TileState tileState in _tileStates)
+        {
+            Destroy(tileState.gameObject);
+        }
+
+        _tileStates = new();
+    }
+    
     void FixedUpdate()
     {
+        if (_timer <= 0)
+        {
+            _smallTimer -= Time.fixedDeltaTime;
+            textTimer.text = "Time's up!";
+            if (_smallTimer > 0)
+                return;
+
+            _smallTimer = timeBetweenRounds;
+            _timer = timePerRound;
+            ClearMap();
+            GenerateMap();
+        }
+        
         int p1Score = 0, p2Score = 0;
-        foreach (var tileState in _tileStates)
+        foreach (TileState tileState in _tileStates)
         {
             EPlayerID owner = tileState.GetOwner();
             switch (owner)
@@ -107,5 +142,8 @@ public class Map : MonoBehaviour
         textPlayer2.text = p2Score.ToString();
         sliderPlayer1.transform.localScale = new Vector3(p1Score/_theThing, 1, 1);
         sliderPlayer2.transform.localScale = new Vector3(p2Score/_theThing, 1, 1);
+        
+        _timer -= Time.fixedDeltaTime;
+        textTimer.text = Mathf.FloorToInt(_timer).ToString();
     }
 }
